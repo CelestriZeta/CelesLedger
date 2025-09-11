6f520de67d117adc77b6791937ea587cf29e200a
Celestri <celestri@163.com>
2025-09-11 23:32:45 +0800

    完成了数据库查询与更新功能

--- src/database.py
+++ src/database.py
@@ -1,37 +1,62 @@
 import sqlite3
+from typing import TypedDict, Annotated, Sequence
 
-class db_manager:
-    def __init__(self, db_name="../consumption_records.db"):
+
+class Record(TypedDict):
+    item: Annotated[str, "所购物品的名称"]
+    cost: Annotated[float, "此次消费或进账的金额，消费记录为负值，进账为正值"]
+    time: Annotated[str, "此次消费或进账发生的时间，以'YYYY-MM-DD'格式记载。请结合用户输入与当前时间，确定消费或进账发生的时间"]
+    type: Annotated[str,
+        """
+        此条记录所属的类型，按照居民消费八大类分类，只能为以下几种：
+        - 食品烟酒
+        - 衣着
+        - 居住
+        - 生活用品及服务
+        - 交通通信
+        - 教育文化娱乐
+        - 医疗保健
+        - 其它用品及服务
+        """
+    ]
+    subtype: Annotated[str, ":此条记录所属的，type所填分类下的子类型"]
+    original_message: Annotated[str, "此条记录对应的用户消息原文"]
+
+db_creation_script = """
+CREATE TABLE IF NOT EXISTS consumption_records
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    item TEXT,
+    cost REAL,
+    time TEXT,
+    type TEXT,
+    subtype TEXT,
+    original_message TEXT
+)
+"""
+
+class DBManager:
+    def __init__(self, db_name="consumption_records.db"):
         self.conn = sqlite3.connect(db_name)
         self.create_table()
 
     def create_table(self):
         with self.conn:
+            self.conn.execute(db_creation_script)
+            self.conn.commit()
+
+    def add_record(self, record: Record):
+        with self.conn:
             self.conn.execute(
                 """
-                CREATE TABLE IF NOT EXISTS consumption_records
-                (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    item TEXT,
-                    cost REAL,
-                    time TEXT,
-                    type TEXT,
-                    subtype TEXT,
-                    original_message TEXT
-                )
-                """
+                INSERT INTO consumption_records (item, cost, time, type, subtype, original_message)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """,
+                (record["item"], record["cost"], record["time"], record["type"], record["subtype"], record["original_message"])
             )
             self.conn.commit()
 
-    def add_record(self, item, cost, time, type, subtype, original_message):
-        with self.conn:
-            self.conn.execute("""
-                INSERT INTO consumption_records (item, cost, time, type, subtype, original_message)
-                VALUES (?, ?, ?, ?, ?, ?)
-            """, (item, cost, time, type, subtype, original_message))
-            self.conn.commit()
-
-    def fetch_records(self):
+    def fetch_all_records(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT * FROM consumption_records")
         return cursor.fetchall()
@@ -40,7 +65,7 @@ class db_manager:
         cursor = self.conn.cursor()
         cursor.execute(query)
         self.conn.commit()
-        return self
+        return cursor.fetchall()
 
     def clear(self):
         with self.conn:
@@ -50,3 +75,4 @@ class db_manager:
     def close(self):
         self.conn.close()
 
+db = DBManager()
--- src/graph.py
+++ src/graph.py
@@ -3,16 +3,28 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
 from langgraph.checkpoint.memory import InMemorySaver
-from src.agent import AgentState, llm
 import datetime
+from src.agent import AgentState, llm
+from src.database import db, Record, db_creation_script
 
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+checkpointer = InMemorySaver()
+"""
+store = InMemoryStore(
+    index={
+        "embed": init_embeddings("openai:text-embedding-3-small"),  # Embedding provider
+        "dims": 1536,                              # Embedding dimensions
+        "fields": ["message"]              # Fields to embed
+    }
+)
+"""
 
 class Behavior(TypedDict):
     key: Annotated[
         str,
         """
-        should be one of 'fetch', 'update' or 'chat'
+        只能为'fetch','update'和'chat'之一;
         'fetch': 用户需要了解消费记录中的内容，应从数据库中检索条目
         'update': 用户的会话中含有提供的消费信息，应更新消费记录数据库
         'chat': 用户的话与消费无关，应进行简单的回复
@@ -36,16 +48,61 @@ def fetch_db_node(state: AgentState) -> AgentState:
     Node to fetch data from the database.
     """
     # fetch data from the database
-    fetched_record = db_manager.invoke(state["messages"][-1:] + [SystemMessage("根据以上消息从数据库中获取消费记录，返回检索到的记录的字符序列。")])
-    return {"messages": [SystemMessage("SystemMessage: 用户可能希望了解的消费记录：" + str(fetched_record.content))]}
+    query = llm.invoke([SystemMessage(
+f"""
+你是一个专业的SQL查询生成器。你的任务是在记录消费的表中，按照用户提供的自然语言描述: {state["messages"][-1]}生成标准且可执行的SQL查询语句，获取一整行的信息。
+
+数据库表的结构如下：{db_creation_script}
+## 数据库表各列描述：
+1. id: primary key
+2. item: 所购物品的名称
+3. cost: 此次消费或进账的金额，消费记录为负值，进账为正值
+4. time: 此次消费或进账发生的时间，以"YYYY-MM-DD"格式记载。当前的时间为{current_time}，请结合用户输入确定消费或进账发生的时间
+5. type: 此条记录所属的类型，按照居民消费八大类分类，只能为以下几种：
+- 食品烟酒
+- 衣着
+- 居住
+- 生活用品及服务
+- 交通通信
+- 教育文化娱乐
+- 医疗保健
+- 其它用品及服务
+6. subtype: 此条记录所属的，type所填分类下的子类型
+7. original_message: 此条记录对应的用户消息原文
+
+注意事项：
+- SQL语句不可修改数据库
+- 只返回SQL查询语句，不要包含任何解释或额外信息
+- 输出中不要包含任何XML标签
+- 输出中不要包含任何Markdown语法
+- 确保SQL语法标准且可在SQLite中执行
+"""
+    ),])
+    print(query.content)
+    try:
+        fetched_record = db.execute(query.content)
+    except Exception as e:
+        print(f"执行SQL查询时出错: {e}")
+    return {"messages": [SystemMessage("SystemMessage: 用户可能希望了解的消费记录：" + str(fetched_record))]}
 
 def update_db_node(state: AgentState) -> AgentState:
     """
     Node to update the database with new consumption information.
     """
     # update the database
-    new_record = db_manager.invoke(state["messages"][-1:] + [SystemMessage("根据以上消息更新数据库中的消费记录，返回所更新的记录条目。")])
-    return {"messages": [SystemMessage("SystemMessage: 消费记录已加入数据库：" + str(new_record))]}
+    structured_llm = llm.with_structured_output(Record)
+    record = structured_llm.invoke([SystemMessage(
+    f"""
+    你是一个专业的消费记录信息提取器。你的任务是按照用户提供的自然语言描述: {state["messages"][-1]}, 总结出此条消费记录各个参数的值。
+    其中，当前的时间为{current_time}。
+    """
+    ),])
+    record["original_message"] = state["messages"][-1].content
+    for k in ["item", "cost", "time", "type", "subtype", "original_message"]:
+        if k not in record:
+            record[k] = None
+    db.add_record(record)
+    return {"messages": [SystemMessage("SystemMessage: 消费记录已加入数据库：" + str(record))]}
 
 def chat_node(state: AgentState) -> AgentState:
     """
@@ -59,10 +116,8 @@ def comment_node(state: AgentState) -> AgentState:
     """
     Node to comment on the consumption record.
     """
-    prompt = "请对上一次SystemMessage中的消费记录进行简短的评论。"
-    state["messages"].append(SystemMessage(prompt))
-    response = llm.invoke(state["messages"])
-    # response.pretty_print()
+    prompt = "请对上一次查询到的或记录的消费记录进行简短的评论。"
+    response = llm.invoke(state["messages"] + [SystemMessage(prompt)])
     return {"messages": [AIMessage(response.content)]}
 
 graph = StateGraph(AgentState)
@@ -84,5 +139,6 @@ graph.add_edge("update_db", "comment")
 graph.add_edge("comment", END)
 graph.add_edge("chat", END)
 
-checkpointer = InMemorySaver()
-app = graph.compile(checkpointer=checkpointer)
+app = graph.compile(
+    checkpointer=checkpointer,
+)
